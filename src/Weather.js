1b408af4837bd89a763d0c895b85c947de61d479
Olya <106160766+OlyaProkofieva@users.noreply.github.com>
2022-09-07 18:46:25 +0300

    Added CSS style

--- src/Weather.js
+++ src/Weather.js
@@ -4,41 +4,53 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <div className="default-cities">
-        <ul>
-          <li>Paris</li>
-          <li>New York</li>
-          <li>Lisbon</li>
-          <li>London</li>
-          <li>Tel Aviv</li>
-          <li>Tokyo</li>
-        </ul>
-      </div>
-      <form>
-        <input type="search" placeholder="Enter a location" />
-        <input type="submit" value="Search" />
-      </form>
-      <div className="row">
-        <div className="col-6">
-          <h1>Kyiv</h1>
-          <ul>
-            <li>September 25, 2022</li>
-            <li>
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                alt="Cloudy"
-              />
-            </li>
-            <li>Cloudy</li>
-          </ul>
+      <div className="weather-app">
+        <div className="row default-cities">
+          <div className="col-2">
+            <a href="/">Paris</a>
+          </div>
+          <div className="col-2">
+            <a href="/">New York</a>
+          </div>
+          <div className="col-2">
+            <a href="/">Lisbon</a>
+          </div>
+          <div className="col-2">
+            <a href="/">London</a>
+          </div>
+          <div className="col-2">
+            <a href="/">Tel Aviv</a>
+          </div>
+          <div className="col-2">
+            <a href="/">Tokyo</a>
+          </div>
         </div>
-        <div className="col-6">
-          <p>28°C</p>
-          <ul>
-            <li>Feels like: 30°C</li>
-            <li>Humidity: 70%</li>
-            <li>Wind: 13 km/h</li>
-          </ul>
+        <form>
+          <input type="search" placeholder="Enter a location" />
+          <input type="submit" value="Search" />
+        </form>
+        <div className="row">
+          <div className="col-8 main-weather-info">
+            <h1>Kyiv</h1>
+            <ul>
+              <li className="current-day">September 25, 2022</li>
+              <li>
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                  alt="Cloudy"
+                />
+              </li>
+              <li className="weather-discription">Cloudy</li>
+            </ul>
+          </div>
+          <div className="col-4 additional-weather-info">
+            <p className="current-temperature">28°</p>
+            <ul>
+              <li>Feels like: 30°C</li>
+              <li>Humidity: 70%</li>
+              <li>Wind: 13 km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
