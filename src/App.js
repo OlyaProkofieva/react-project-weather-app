157190e9593506c1203cea98e41dccea61b7a1b9
Olya <106160766+OlyaProkofieva@users.noreply.github.com>
2022-09-07 13:40:21 +0300

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
-      <p>
-        This project was coded by Olha Prokofieva and it's open-sourced on{" "}
-        <a href="https://github.com/OlyaProkofieva/react-project-weather-app">
-          GitHub
-        </a>
-      </p>
+      <div className="container">
+        <h1>Hello!</h1>
+        <footer>
+          <p>
+            This project was coded by Olha Prokofieva and it's open-sourced on{" "}
+            <a href="https://github.com/OlyaProkofieva/react-project-weather-app">
+              GitHub
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
