afd73591aeaf1a53bc173d29c5d5f17751288ecc
Olya <106160766+OlyaProkofieva@users.noreply.github.com>
2022-09-07 11:06:53 +0300

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
+      <p>
+        This project was coded by Olha Prokofieva and it's open-sourced on{" "}
+        <a href="https://github.com/OlyaProkofieva/react-project-weather-app">
+          GitHub
+        </a>
+      </p>
     </div>
   );
 }
